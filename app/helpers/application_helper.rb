475a7a943bf47e1d8882a34ac77dfb293b4194cf
Guy Newman <guy@weblitz.co.uk>
2010-06-01 12:59:21 +0100

    Fix to superfish menu styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,18 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def selected_element_if(condition, element, attributes = {}, & block)
+  def selected_element_if(element, condition, attributes = {}, & block)
     attributes['class'] = 'selected' if condition
     content_tag(element, attributes, & block)
   end
 
-  def build_menu(sections, selected = nil)
-    sections.collect do |section|
-      selected_element_if(section == selected, :li) do
-        link_to(section.name, section) <<
-                content_tag(:ul) do
-                  build_menu(section.children, selected)
-                end if section.children
+  def build_menu(sections, selected = nil, options = {})
+    content_tag(:ul, :class => options[:class]) do
+      sections.collect do |section|
+        selected_element_if(:li, section == selected) do
+          li = link_to(section.name, section)
+          li << build_menu(section.children, selected) if section.children.count > 0
+          li
+        end
       end
     end
   end
